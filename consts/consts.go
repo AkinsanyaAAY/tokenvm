ff11c7ff57a28116807f949e5b6cac9ff9b4c9f6
AkinsanyaAAY <akinsanyaayomide0108@gmail.com>
2024-07-10 15:09:04 +0000

    origin

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "AKINSANYA-HRP"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SANYA"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ASY"
 )
 
 var ID ids.ID
